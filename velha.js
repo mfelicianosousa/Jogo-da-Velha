a31bdbeb2110bace5d13f383c3cfc4bf33ccfc92
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-04-11 09:09:02 -0400

    Iniciando a dinamica do jogo

---
+++ velha.js
@@ -0,0 +1,16 @@
+var jogador = null ;
+var jogadorSelecionado = document.getElementById('jogador-selecionado') ;
+mudarJogador('X')
+
+function escolherQuadrado( id ){
+  var quadrado = document.getElementById(id) ;
+  quadrado.innerHTML = jogador;
+  quadrado.style.color = '#000';
+  
+}
+
+function mudarJogador(valor){
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador ;
+
+}
